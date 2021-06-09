bd2de28b15d0ec88d30120a46793ebd7fff65773
NASSRI Hamdi <nassrihamdi@gmail.com>
2021-06-09 15:29:26 +0200

    affichage de tous les personnages par pagination

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import PeopleAll from "./components/PeopleAll";
 
 function App() {
--- src/components/PeopleAll.js
+++ src/components/PeopleAll.js
@@ -1,9 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+
 
 function PeopleAll() {
-    return(
-        <h1>Ça marche!!!</h1>
-    )
+    const [people, setPeople] = useState();
+    async function fetchPeople(link = "https://swapi.dev/api/people/"){
+        const people_all = await axios.get(link);
+        setPeople(people_all);
+    }
+    useEffect(() => {
+        fetchPeople(); 
+    }, [])
+    if(people){
+        return(
+            <div className="PeopleAll">
+                { people.data.previous !== null && <button onClick={() => fetchPeople(people.data.previous)}>Précédent</button>}
+                { people.data.next !== null && <button onClick={() => fetchPeople(people.data.next)}>Suivant</button>}
+                { people.data.results.map((element, index) => {
+                    return (
+                        <h3 key={index}>{element.name}</h3>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    return null;
+    
 }
 
 export default PeopleAll;
